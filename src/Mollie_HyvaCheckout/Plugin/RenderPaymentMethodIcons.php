84ceb9a6271cb39986c2b27e48ac0f9221e31211
Tjitse Efdé <tjitse@vendic.nl>
2023-07-11 21:26:33 +0200

    Check if useImage is true, bump hyva checkout requirement

--- src/Mollie_HyvaCheckout/Plugin/RenderPaymentMethodIcons.php
+++ src/Mollie_HyvaCheckout/Plugin/RenderPaymentMethodIcons.php
@@ -3,9 +3,17 @@
 namespace Mollie\HyvaCheckout\Plugin;
 
 use Hyva\Checkout\Model\MethodMetaData;
+use Mollie\Payment\Helper\General as MollieHelper;
 
 class RenderPaymentMethodIcons
 {
+    private MollieHelper $mollieHelper;
+
+    public function __construct(MollieHelper $mollieHelper)
+    {
+        $this->mollieHelper = $mollieHelper;
+    }
+
     /**
      * Mollie payments do not have meta-data icons, so we need to make sure the icons are rendered without the
      * meta-data being set. The actual data will be populated by the PopulateIconData plugin.
@@ -13,6 +21,10 @@ class RenderPaymentMethodIcons
      */
     public function afterCanRenderIcon(MethodMetaData $subject, $result)
     {
+        if (!$this->mollieHelper->useImage()) {
+            return $result;
+        }
+
         if (strpos($subject->getMethod()->getCode(), 'mollie_methods_') === 0) {
             return true;
         }
